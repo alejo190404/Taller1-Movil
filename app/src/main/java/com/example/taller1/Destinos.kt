28fbeddff106bb9cd71c419f962719750c67eb02
alejo190404 <alejo190404@gmail.com>
2024-08-28 02:26:44 -0500

    Pantalla con filtrado funcional. No cumple con requerimiento de máximo 20 líneas en el método onCreate()

--- app/src/main/java/com/example/taller1/Destinos.kt
+++ app/src/main/java/com/example/taller1/Destinos.kt
@@ -1,6 +1,7 @@
 package com.example.taller1
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
@@ -12,10 +13,13 @@ import java.io.IOException
 import java.io.InputStream
 
 class Destinos : AppCompatActivity() {
+    lateinit var opcion:Any
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_destinos)
+
+        opcion = intent.getStringExtra("opcion").toString()
 
         val json = JSONObject(loadJSONFromAsset())
         val destinos = json.getJSONArray("destinos")
@@ -23,15 +27,27 @@ class Destinos : AppCompatActivity() {
         //Arreglo (tamaño estático)
         val arreglo = arrayOfNulls<String>(destinos.length())
 
-        for (i in 0 until destinos.length()){
-            val jsonObject = destinos.getJSONObject(i)
-            arreglo[i]=(jsonObject.getString("nombre"))
+        if (opcion == "Todos"){
+            for (i in 0 until destinos.length()){
+                val jsonObject = destinos.getJSONObject(i)
+                arreglo[i]=(jsonObject.getString("nombre"))
+            }
+        }
+        else {
+            for (i in 0 until destinos.length()){
+                val jsonObject = destinos.getJSONObject(i)
+//                Log.i("DEVELOPER BREAKPOINT", jsonObject.getString("categoria"))
+                if (jsonObject.getString("categoria") == opcion){
+                    arreglo[i]=(jsonObject.getString("nombre"))
+                }
+            }
         }
 
+        val arregloFiltrado = arreglo.filterNotNull()
         val lista = findViewById<ListView>(R.id.listView)
         val adaptador = ArrayAdapter(baseContext, //Contexto
             android.R.layout.simple_list_item_1, //Forma visual
-            arreglo
+            arregloFiltrado
         )
         lista.adapter = adaptador
     }
--- app/src/main/java/com/example/taller1/MainActivity.kt
+++ app/src/main/java/com/example/taller1/MainActivity.kt
@@ -2,25 +2,46 @@ package com.example.taller1
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    lateinit var opcion: Any
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        spinner.onItemSelectedListener = this
+
+
+
         val botonExplorar:Button = findViewById(R.id.botonExplorar)
         val botonFavoritos:Button = findViewById(R.id.botonFavoritos)
         val botonRecomendaciones:Button = findViewById(R.id.botonRecomendaciones)
 
+
+
         botonExplorar.setOnClickListener{
-            startActivity(Intent(this, Destinos::class.java))
+            val intent = Intent(this, Destinos::class.java)
+            intent.putExtra("opcion", opcion.toString())
+            startActivity(intent)
         }
 
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        opcion = parent!!.selectedItem
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        opcion = parent!!.getItemAtPosition(0)
     }
 }
