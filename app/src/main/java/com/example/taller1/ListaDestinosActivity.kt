866a710a02d365b5f77df74c8004d16dccdd7902
alejo190404 <alejo190404@gmail.com>
2024-08-29 00:44:21 -0500

    Cambios en el código para que funcione con companion objects en vez de pasar bundles para cada intent

--- app/src/main/java/com/example/taller1/ListaDestinosActivity.kt
+++ app/src/main/java/com/example/taller1/ListaDestinosActivity.kt
@@ -2,6 +2,7 @@ package com.example.taller1
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -17,44 +18,31 @@ class ListaDestinosActivity : AppCompatActivity() {
     lateinit var opcion:Any
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_lista_destinos)
+
         opcion = intent.getStringExtra("opcion").toString()
 
-        val json = JSONObject(loadJSONFromAsset())
-        val destinos = json.getJSONArray("destinos")
-
-        var arreglo = arrayOfNulls<String>(destinos.length())
-
-        arreglo = llenarArreglo(arreglo, destinos)
+        var arregloNombres = arrayOfNulls<String>(Data.ARREGLO_DESTINOS.length())
+        arregloNombres = llenarArreglo(arregloNombres, Data.ARREGLO_DESTINOS)
 
         val lista = findViewById<ListView>(R.id.listView)
         val adaptador = ArrayAdapter(baseContext, //Contexto
             android.R.layout.simple_list_item_1, //Forma visual
-            arreglo.filterNotNull()
+            arregloNombres.filterNotNull()
         )
         lista.adapter = adaptador
 
-        lista.setOnItemClickListener(object: AdapterView.OnItemClickListener{
-            override fun onItemClick( parent: AdapterView<*>?,  view: View?, position: Int, id: Long) {
-                crearIntentDetalles(encontrarJSONObject(arreglo[position], destinos))
+        lista.setOnItemClickListener(object : AdapterView.OnItemClickListener {
+            override fun onItemClick(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position) as? String
+                selectedItem?.let { crearIntentDetalles(it) }
             }
-
         })
     }
 
-    private fun encontrarJSONObject(nombre: String?, destinos: JSONArray): JSONObject{
-        for (i in 0 until destinos.length()){
-            if (destinos.getJSONObject(i).getString("nombre") == nombre){
-                return destinos.getJSONObject(i)
-            }
-        }
-        return JSONObject()
-    }
-
-    private fun crearIntentDetalles(destino: JSONObject) {
+    private fun crearIntentDetalles(nomDestino: String) {
         val intent = Intent(this, DetalleDestinosActivity::class.java)
-        intent.putExtra("destino", destino.toString())
+        intent.putExtra("destino", nomDestino)
         startActivity(intent)
     }
 
